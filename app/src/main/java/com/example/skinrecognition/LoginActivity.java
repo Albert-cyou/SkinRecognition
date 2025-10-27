49e4834c82244e6d0cfa003ac6a85ba3a55d81bd
Albert-cyou <zhangchen2123@mails.jlu.edu.cn>
2025-10-27 11:20:59 +0800

    更新了页面跳转逻辑

--- app/src/main/java/com/example/skinrecognition/LoginActivity.java
+++ app/src/main/java/com/example/skinrecognition/LoginActivity.java
@@ -1,6 +1,11 @@
-package com.example.skinrecognition;
+ package com.example.skinrecognition;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button wx_login, qq_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,33 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        wx_login = findViewById(R.id.wechatLogin);
+        qq_login = findViewById(R.id.qqLogin);
+
+        wx_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "微信登录成功", Toast.LENGTH_SHORT).show();
+                Log.e("LOGIN","use wx login successfully");
+                goToMain("微信用户");
+            }
+        });
+
+        qq_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "QQ登录成功", Toast.LENGTH_SHORT).show();
+                Log.e("LOGIN","use qq login successfully");
+                goToMain("QQ用户");
+            }
+        });
+    }
+
+    // 跳转到主界面
+    private void goToMain(String username) {
+        Intent intent = new Intent(LoginActivity.this, PredictActivity.class);
+        intent.putExtra("username", username); // 传递用户名
+        startActivity(intent);
     }
 }
